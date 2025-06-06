b638cf88099c435da2328141e9a387f1dac223b4
tomcabanau <tom.cabanau@gmail.com>
2025-06-06 14:42:12 +0200

    style_recherche

--- front/views/client/recherche.php
+++ front/views/client/recherche.php
@@ -1,17 +1,18 @@
 <div class="page-recherche">
     <h2>Recherche</h2>
-    <form id="rechercheForm">
-        <select class="form-select mb-3" name="onduleur">
+    <form id="rechercheForm" class="form-ligne">
+        <select class="form-select" name="onduleur">
             <option value="">Marque onduleur</option>
         </select>
-        <select class="form-select mb-3" name="panneau">
+        <select class="form-select" name="panneau">
             <option value="">Marque panneau</option>
         </select>
-        <select class="form-select mb-3" name="departement">
+        <select class="form-select" name="departement">
             <option value="">Département</option>
         </select>
         <button type="submit" class="btn btn-primary">Rechercher</button>
     </form>
+
 
     <div id="resultats"></div>
     <div id="pagination" class="pagination mt-3"></div>
